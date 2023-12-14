5852665e1d8c251bbe46b461cb5485126fba8ca6
inalalkalas <43957675+inalalkalas@users.noreply.github.com>
2023-12-14 17:32:54 +0700

    Add files via upload

--- projectUAS.c
+++ projectUAS.c
@@ -2,52 +2,34 @@
 #include <ctype.h>
 #include <string.h>
 #include <libpq-fe.h>
+#include ""
 
 // struct pada data
 struct All [2][2] {
     {char namaBarangPenjual [50], char namaBarangPembeli [50]}
     {int hargaBarangJual, int hargaBarangBeli}
-} 
+}   
+
+// perulangan untuk kembali ke menu utama
+void tandaReturn(){
+    if (... == 'r' || ... = 'R'){
+        return 
+    } else {
+        return 1
+    }
+}
+
+// perulangan untuk exit 
+void tandaExit(){
+    if (... == 'e' || ... = 'E'){
+        return 1;
+    }
+}
 
 // membersihkan enter
 void clearNewline() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
-}
-
-// menampilkan data pada pilihan pertama
-void tampilanData (){
-    printf("Login Sebagai:\n");
-    printf("1. Penjual\n");
-    printf("2. Pembeli\n");
-    printf("E. Exit\n");
-    printf("Pilihan Anda: ");    
-} 
-
-// menampilkan data pada penjual
-void tampilSbgPenjual () {
-    printf("1. Tambahkan Item\n");
-    printf("2. ");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-
-}
-
-// menampilkan data pada pembeli
-void tampilSbgPembeli() {
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-    printf("");
-
 }
 
 // mengurutkan harga dari yang terendah
@@ -60,21 +42,106 @@ void hargaTerendah(struct Produk *produk, int jumlahProduk, int descending){
 
 }
 
-void tampilanKeranjang(){
+// menampilkan data pada pilihan pertama
+void tampilanData (){
+    printf("Login Sebagai:\n");
+    printf("1. Penjual\n");
+    printf("2. Pembeli\n");
+    printf("E. Exit\n");
+    printf("Pilihan Anda: ");
+    scanf("");
+
+    if(n == 'e'|| n == 'E'){
+        return 1;
+    }
+} 
+
+// menampilkan data pada penjual
+void tampilSbgPenjual () {
+    printf("1. Tambahkan Item\n");
+    printf("2. lihat data produk\n");
+    printf("3. pesan\n");
+    printf("4. notifikasi\n");
+    printf("5. setting\n");
+    printf("E. exit\n");
+    scanf("");
+   
+    if (n)
+}
+
+// menambahkan item untuk penjual
+void tambahItemPenjual(){
 
 }
 
+// menampilkan barang yang sudah ditambahkan
+void tampilItem(){
+
+}
+
+// menampilkan pesan yang mengarah ke pembeli
+void pesanPenjual(){
+
+}
+
+// menampilkan notifikasi apa yang terjadi
+void notifPenjual(){
+
+}
+
+// menampilkan settingan untuk penjual melengkapi biodata
+void settingPenjual(){
+
+}
+
+
+// menampilkan data pada pembeli
+void tampilSbgPembeli() {
+    printf("1. searching\n");
+    printf("2. keranjang\n");
+    printf("3. pesan\n");
+    printf("4. notifikasi\n");
+    printf("5. setting\n");
+    printf("E. exit");
+    scanf("");
+}
+
+// mencari barang untuk pembeli
+void tampilSearchingPembeli(){
+    printf("Search ");
+    scanf("", );
+
+    
+}
+
+// menampilkan isi pada keranjang 
+void tampilanKeranjang(){
+
+
+}
+
+// menampilkan profil dan juga data tentang penjual
 void tampilanProfilePenjual(){
 
 }
 
+// menampilkan baarang yang sudah di cari
 void tampilSearchBarang(){
 
 }
 
-void tampilBarangPembeli(){
+// menampilkan pesan yang mengarah ke pembeli
+void pesanPembeli(){
 
 }
 
-void 
+// menampilkan notifikasi apa yang terjadi
+void notifPembeli(){
+
+}
+
+// menampilkan settingan untuk penjual melengkapi biodata
+void settingPembeli(){
+
+}
 
